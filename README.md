0ab4bf55eb6c2cd368e3ebc198095f4756f45136
zwk1091 <zhuwk3@mail2.sysu.edu.cn>
2019-12-10 21:45:09 +0800

    add raw data and instruction

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CarNews_InformationExtract
+
+[原始数据NewCar_new](NewsCar_new)
+
+运行preprocess.py得到分句的数据
+
+运行preprocess_cutword.py得到分词的数据
+
+word2vec表示获取词向量的代码
--- word_embeding_by_word2vec.py
+++ word_embeding_by_word2vec.py
@@ -1,5 +1,22 @@
 from gensim.models import word2vec
 
-word2vec.word2vec('../NewsCar_new/NewsCar_new/1.txt','res.bin',size=50,verbose=True)
+# 加载分词后的文件
+sentences=word2vec.Text8Corpus('../NewsCar_new_after_cutWord/1/2.txt')
+
+# size 表示词向量维度 min_count表示最小出现次数
+model=word2vec.Word2Vec(sentences,size=100,min_count=1)
+
+# 计算和动力最相似的5个词
+x=model.most_similar("动力",topn=5)
+print (x)
+
+# 输出'汽车'的词向量
+print(model['汽车'])
+
+# 保存模型
+model.save("res.model")
+# 对应的加载方式
+# model_2 = word2vec.Word2Vec.load("text8.model")
+
 
 
